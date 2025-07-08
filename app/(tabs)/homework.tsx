f184ec589a33498e61675540c408e0e14b1f7b25
u200b <uwu@owo.computer>
2025-07-08 08:37:42 +0200

    fix: move components and stuff out of the app dir to fix errors with async stuff in the app dir interpreted as route

--- app/(tabs)/homework.tsx
+++ app/(tabs)/homework.tsx
@@ -1,40 +1,40 @@
-import { FlatList, StyleSheet, Text, View } from 'react-native';
-import AddButton from '../components/Buttons/AddButton';
-import HomeworkBox from '../components/HomeworkBox';
-import getHomework from '@/app/Service/getHomework';
-import formatDate from '../Service/Date/formatDate';
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import AddButton from '@/components/Buttons/AddButton'
+import HomeworkBox from '@/components/HomeworkBox'
+import getHomework from '@/service/getHomework'
+import formatDate from '@/service/Date/formatDate'
 
 export default function Homework() {
-  const { error, Homework } = getHomework();
+    const {error, Homework} = getHomework()
 
-  return (
-    <View style={styles.container}>
-      {error && <Text>Error: {error}</Text>}
+    return (
+        <View style={styles.container}>
+            {error && <Text>Error: {error}</Text>}
 
-      <FlatList
-        data={Homework}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <HomeworkBox
-            Subject={item.Subjekt}
-            Text={item.to_do}
-            date={formatDate(item.due)}
-          />
-        )}
-        contentContainerStyle={styles.listContent}
-      />
+            <FlatList
+                data={Homework}
+                keyExtractor={( item ) => item.id.toString()}
+                renderItem={( {item} ) => (
+                    <HomeworkBox
+                        Subject={item.Subjekt}
+                        Text={item.to_do}
+                        date={formatDate(item.due)}
+                    />
+                )}
+                contentContainerStyle={styles.listContent}
+            />
 
-      <AddButton />
-    </View>
-  );
+            <AddButton/>
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  listContent: {
-    padding: 20,
-    paddingBottom: 100,
-  },
-});
+    container: {
+        flex: 1,
+    },
+    listContent: {
+        padding: 20,
+        paddingBottom: 100,
+    },
+})
