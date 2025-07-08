d298627b7eec03952cd5230427729e9841c866db
Noa Santo <72336443+virus-rpi@users.noreply.github.com>
2025-07-08 08:07:20 +0200

    Update app/(tabs)/_layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,6 +1,6 @@
 import { FontAwesome } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
-import React, { useState, useRef } from 'react';
+import React from 'react';
 
 function TabBarIcon(props: { name: React.ComponentProps<typeof FontAwesome>['name']; color: string }) {
   return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
