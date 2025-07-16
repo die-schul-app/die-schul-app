0da46d10aa06a44596d88dd2abeb59d908d5a906
Raphael <raphael.kastenmeier@gmail.com>
2025-07-16 11:54:49 +0200

    removed home tab

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,62 +1,55 @@
-import { Colors } from '@/constants/Colors';
-import { useTheme } from '@/contexts/ThemeContext';
-import { FontAwesome } from '@expo/vector-icons';
-import { Tabs } from 'expo-router';
+import {Colors} from '@/constants/Colors';
+import {useTheme} from '@/contexts/ThemeContext';
+import {FontAwesome} from '@expo/vector-icons';
+import {Tabs} from 'expo-router';
 import React from 'react';
 
 function TabBarIcon(props: { name: React.ComponentProps<typeof FontAwesome>['name']; color: string }) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+    return <FontAwesome size={28} style={{marginBottom: -3}} {...props} />;
 }
 
 export default function TabLayout() {
-  const { theme } = useTheme();
-  const colors = theme === 'light' ? Colors.light : Colors.dark;
+    const {theme} = useTheme();
+    const colors = theme === 'light' ? Colors.light : Colors.dark;
 
-  return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: colors.tint,
-        tabBarStyle: { backgroundColor: colors.background },
-        headerStyle: {
-          backgroundColor: colors.background,
-        },
-        headerTintColor: colors.text,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="schedule"
-        options={{
-          title: 'Schedule',
-          tabBarIcon: ({ color }) => <TabBarIcon name="calendar" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="homework"
-        options={{
-          title: 'Homework',
-          tabBarIcon: ({ color }) => <TabBarIcon name="book" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="grades"
-        options={{
-          title: 'Grades',
-          tabBarIcon: ({ color }) => <TabBarIcon name="star" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="settings"
-        options={{
-          title: 'Settings',
-          tabBarIcon: ({ color }) => <TabBarIcon name="cog" color={color} />,
-        }}
-      />
-    </Tabs>
-  );
+    return (
+        <Tabs
+            screenOptions={{
+                tabBarActiveTintColor: colors.tint,
+                tabBarStyle: {backgroundColor: colors.background},
+                headerStyle: {
+                    backgroundColor: colors.background,
+                },
+                headerTintColor: colors.text,
+            }}>
+            <Tabs.Screen
+                name="schedule"
+                options={{
+                    title: 'Schedule',
+                    tabBarIcon: ({color}) => <TabBarIcon name="calendar" color={color}/>,
+                }}
+            />
+            <Tabs.Screen
+                name="homework"
+                options={{
+                    title: 'Homework',
+                    tabBarIcon: ({color}) => <TabBarIcon name="book" color={color}/>,
+                }}
+            />
+            <Tabs.Screen
+                name="grades"
+                options={{
+                    title: 'Grades',
+                    tabBarIcon: ({color}) => <TabBarIcon name="star" color={color}/>,
+                }}
+            />
+            <Tabs.Screen
+                name="settings"
+                options={{
+                    title: 'Settings',
+                    tabBarIcon: ({color}) => <TabBarIcon name="cog" color={color}/>,
+                }}
+            />
+        </Tabs>
+    );
 } 
